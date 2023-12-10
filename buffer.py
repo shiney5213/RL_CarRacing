bf8fe02fe460fa34b0b55d8cd1f405dcdaf07011
shiney5213 <shiney5213@gmail.com>
2023-12-10 17:45:22 +0900

    sample method output type

--- buffer.py
+++ buffer.py
@@ -5,8 +5,9 @@ import torch
 
 
 class ReplayBuffer():
-    def __init__(self, state_dim, action_dim, max_size):
+    def __init__(self, state_dim, action_dim, max_size, device):
         self.buffer = collections.deque(maxlen = max_size)
+        self.device = device
         
     def size(self):
         return len(self.buffer)
@@ -15,16 +16,19 @@ class ReplayBuffer():
         # transition(s, a, r, s')
         self.buffer.append(transition)
 
-    def sample(self, n):
-        # 50000개 중 minibatch size만큼 random으로 빼기
-        mini_batch = random.sample(self.buffer, n)
 
+
+    def sample(self, batch_size):
+        # 50000개 중 minibatch size만큼 random으로 빼기
+        mini_batch = random.sample(self.buffer, batch_size)   
+        # print('mini_batch', len(mini_batch))         # mini_batch
+        # print(mini_batch[0][0].shape)                # [4, 84, 84]
 
         s_lst, a_lst, r_lst, s_prime_lst, done_mask_lst = [], [], [], [], []
 
         for transition in mini_batch:
-            s, a, r, s_prime, done_mask = transition
-
+            s, a, r, s_prime, done_mask = transition     # ndarray, int, float, ndarray, float
+            
             # s, s_prime : [4, 84, 84] array/ a, r: scala value
             s_lst.append(s)
             a_lst.append([a])
@@ -32,14 +36,14 @@ class ReplayBuffer():
             s_prime_lst.append(s_prime)
             done_mask_lst.append([done_mask])
 
+        s_lst = np.array(s_lst)
+        s_prime_lst = np.array(s_prime_lst)
         # dtype : 숫자형 변환
-        return torch.tensor(s_lst, dtype = torch.float), \
-                torch.tensor(a_lst),\
-                torch.tensor(r_lst),\
-                torch.tensor(s_prime_lst, dtype = torch.float),\
-                torch.tensor(done_mask_lst)
-
-
-
-
+        return torch.tensor(s_lst, dtype=torch.float).to(self.device),\
+               torch.tensor(a_lst).to(self.device), \
+               torch.tensor(r_lst).to(self.device), \
+               torch.tensor(s_prime_lst, dtype=torch.float).to(self.device), \
+               torch.tensor(done_mask_lst).to(self.device)
+    
+    
 
