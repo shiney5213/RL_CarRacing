3fea56f2a05398e8b9729ff62dbeaf1c85320aea
shiney5213 <shiney5213@gmail.com>
2023-12-10 17:46:14 +0900

    feat : save model, save return image

--- utils.py
+++ utils.py
@@ -2,7 +2,58 @@ import numpy as np
 import os
 import cv2
 import pygame
+import matplotlib.pyplot as plt
+import torch
 
+def save_model(model, n_epi, t, score, optimizer, dir_path, filename, model_type):
+
+    score = round(score, 3)
+    checkpoint_path = os.path.join(dir_path, f'{filename}_{model_type}_{n_epi}_{t}_{score}.pt')
+    state =  {
+        'episode': n_epi,
+        'score': score,
+        'model': model.state_dict(),               # model weight 저
+        'step': t, 
+        'optimizer': optimizer.state_dict()
+        }
+
+    torch.save(state, checkpoint_path)
+
+def save_returngraph(return_list, dir_path, filename, n_episode, learning_rate):
+    plt.plot(return_list)
+    plt.xlabel('Iteration')
+    plt.ylabel('Return')
+
+    plt.savefig(os.path.join(dir_path, f'{filename}_{n_episode}.png'), format='png', dpi=300)
+
+def plot_durations(episode_durations, dir_path, filename, n_episode, show_result=False):
+    """
+    지난 100개 에피소드의 평균(공식 평가에서 사용 된 수치)에 따른 에피소드의 지속을 도표화
+    """
+    plt.figure(1)
+    durations_t = torch.tensor(episode_durations, dtype=torch.float)
+    if show_result:
+        plt.title('Result')
+    else:
+        plt.clf()
+        plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(durations_t.numpy())
+    # 100개의 에피소드 평균을 가져 와서 도표 그리기
+    if len(durations_t) >= 100:
+        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+        means = torch.cat((torch.zeros(99), means))
+        plt.plot(means.numpy())
+
+    plt.savefig(os.path.join(dir_path, f'{filename}_{n_episode}_durations.png'), format='png', dpi=300)
+    # plt.pause(0.001)  # 도표가 업데이트되도록 잠시 멈춤
+    # if is_ipython:
+    #     if not show_result:
+    #         display.display(plt.gcf())
+    #         display.clear_output(wait=True)
+    #     else:
+    #         display.display(plt.gcf())
 
 
 
