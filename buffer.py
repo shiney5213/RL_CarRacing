efd709d6fd74bebf3cea782cd456d5c7a3ee6c2f
shiney5213 <shiney5213@gmail.com>
2023-12-11 15:07:00 +0900

    fix : a_lst type

--- buffer.py
+++ buffer.py
@@ -5,7 +5,8 @@ import torch
 
 
 class ReplayBuffer():
-    def __init__(self, state_dim, action_dim, max_size, device):
+    def __init__(self, state_dim, action_dim, max_size, device, seed):
+        random.seed(seed)
         self.buffer = collections.deque(maxlen = max_size)
         self.device = device
         
@@ -31,13 +32,15 @@ class ReplayBuffer():
             
             # s, s_prime : [4, 84, 84] array/ a, r: scala value
             s_lst.append(s)
-            a_lst.append([a])
+            # a_lst.append([a])
+            a_lst.append(a)
             r_lst.append([r])
             s_prime_lst.append(s_prime)
             done_mask_lst.append([done_mask])
 
         s_lst = np.array(s_lst)
         s_prime_lst = np.array(s_prime_lst)
+        a_lst = np.array(a_lst)
         # dtype : 숫자형 변환
         return torch.tensor(s_lst, dtype=torch.float).to(self.device),\
                torch.tensor(a_lst).to(self.device), \
