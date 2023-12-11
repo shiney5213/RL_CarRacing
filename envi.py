3991d3d01f93a9a7a3f49f492d189e60de61d5b5
shiney5213 <shiney5213@gmail.com>
2023-12-11 15:07:55 +0900

    feat : image preprocessing(crop, gray, blur ect)

--- envi.py
+++ envi.py
@@ -2,16 +2,38 @@ import gymnasium as gym
 import numpy as np
 import cv2
 
+from preprocessing import green_mask, gray_scale, blur_image, canny_edge_detector
+from utils import image_save
 
 def preprocess(img):
     # preprocessing in DQN paper
     # img -> grascale -> [1, 84, 84] -> normalization
-    img = img[:84, 6:90]
+    image_save(img, './result/preprocess', '0.img.png')
 
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)/ 255.0
+    # image size : 84 * 84
+    croped = img[:84, 6:90]
+    # image_save(croped, './result/preprocess', '1.croped.png')
+
+    # 배경만 남기기
+    green = green_mask(croped)
+    # image_save(green, './result/preprocess', '2.green.png')
+
+    # grayscale
+    grey  = gray_scale(green)
+    # image_save(grey, './result/preprocess', '3.gray.png')
+
+    # 노이즈 없애기
+    blur  = blur_image(grey)
+    # image_save(blur, './result/preprocess', '4.blur.png')
+
+    # 가장자리 감지
+    canny = canny_edge_detector(blur)
+    # image_save(canny, './result/preprocess', '5.canny.png')
+
+    #  normalization
+    img = canny/ 255.0
 
     return img
-
 
 
 
@@ -26,12 +48,12 @@ class SetEnv(gym.Wrapper):
 
     """
 
-    def __init__(self, env, skip_frames = 4, stack_frames = 4, initial_no_op= 50, **kwargs):
+    def __init__(self, env, is_continuous, skip_frames = 4, stack_frames = 4, initial_no_op= 50,  **kwargs):
         super(SetEnv, self).__init__(env, **kwargs)
         self.inital_no_op = initial_no_op
         self.skip_frames = skip_frames
         self.stack_frames= stack_frames
-        # self.stacked_frame 
+        self.is_continuous = is_continuous
     
     def reset(self):
 
@@ -40,8 +62,12 @@ class SetEnv(gym.Wrapper):
 
         # 초반 50번의 step:  pygame window에서 zoom하는 과정 -> 아무것도 하지 않도록 action = 0
         # error 발생
+        if self.is_continuous:
+            init_action = (0, 0, 0)
+        else:
+            init_action = 0
         for i in range(self.inital_no_op):
-            s, r, terminated, truncated, info = self.env.step(0)
+            s, r, terminated, truncated, info = self.env.step(init_action)
             
         s = preprocess(s)
 
@@ -58,6 +84,7 @@ class SetEnv(gym.Wrapper):
         # history frame 4개 쌓아서 제공
         reward = 0
         for _ in range(self.skip_frames):
+            # print('action', action)
             s, r, terminated, truncated, info = self.env.step(action)
             reward += r
             if terminated or truncated:
@@ -69,3 +96,4 @@ class SetEnv(gym.Wrapper):
         return self.stacked_frame, reward, terminated, truncated, info
 
     
+
